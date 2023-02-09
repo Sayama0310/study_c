4966fcd34817f4d670bfa6805afb4385d4d1ea6a
Sayama0310 <montblanc3060@gmail.com>
2023-02-09 23:29:23 +0900

    [feat] 様々な数のタイプ、リテラルを使用

---
+++ ch04/int_type.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("10 : %d\n", 10); // 10進数
+    printf("010 : %d\n", 010); // 8進数
+    printf("0x10 : %d\n", 0x10); // 16進数
+    printf("10.0 : %f\n", 10.0); // 小数点
+    return 0;
+}
