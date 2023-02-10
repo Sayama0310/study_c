68bbefc45b527a71768ff9491a148e4316640d12
Sayama0310 <montblanc3060@gmail.com>
2023-02-10 18:46:40 +0900

    [feat] 桁を揃えて出力させる

---
+++ ch05/digit_print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("%5d\n", 1);
+    printf("%5d\n", 19);
+    printf("%5d\n", 1997);
+
+    printf("%05d\n", 1);
+    printf("%05d\n", 19);
+    printf("%05d\n", 1997);
+}
